a2d936df0d4d92acfba6fb3666df3f8c081e4f89
withtahmid <withtahmid@withtahmid.withtahmid>
2023-06-27 23:41:28 +0600

    slight modifications

--- video/js/adjustratio.js
+++ video/js/adjustratio.js
@@ -2,9 +2,7 @@ function adjust(){
 	height = (window.innerHeight/2) - 13;
 	boxes = document.getElementsByClassName('box');
 	for(i = 0; i < boxes.length; ++i){
-		box = boxes[i];
-		// box.style.width = width + 'px';
-		box.style.height = height + 'px';
+		boxes[i].style.height = height + 'px';
 	}
 }
 window.addEventListener('resize', adjust);
