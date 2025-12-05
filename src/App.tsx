d7fdb25ef9a9d33b76b8edfc6e1cfc2689f52f90
withtahmid <withtahmid@gmai.com>
2025-12-06 03:56:07 +0600

    BAL

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import ToastProvider from "./Components/ToastProvider";
 import Portfolio from "./Pages/Prortfolio";
+import Router from "./Router";
 
 const App = () => {
-    return <Portfolio />;
+    // return <Portfolio />;  return (
+    return (
+        <ToastProvider>
+            <Router />
+        </ToastProvider>
+    );
 };
 export default App;
